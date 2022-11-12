9d70c7ab0eab6f595f09a57059aed0cff5db11c3
xxseva44 <dev.xxseva44@gmail.com>
2022-11-11 23:30:47 -0500

    TEMP:Kill radio
    At the moment it is not functional and spams logs with various errors. Let's not waste perf by spamming logs.

--- msm8996.mk
+++ msm8996.mk
@@ -339,17 +339,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
-# RIL
+# RIL Not functional at the moment.
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
-    android.hardware.radio@1.4.vendor \
-    android.hardware.radio.config@1.1 \
-    android.hardware.radio.config@1.1.vendor \
-    android.hardware.radio.deprecated@1.0 \
-    android.hardware.radio.deprecated@1.0.vendor \
-    libprotobuf-cpp-full \
+libprotobuf-cpp-full \
     librmnetctl 
-
+#    android.hardware.radio@1.4 \
+#    android.hardware.radio@1.4.vendor \
+#    android.hardware.radio.config@1.1 \
+#    android.hardware.radio.config@1.1.vendor \
+#    android.hardware.radio.deprecated@1.0 \
+#    android.hardware.radio.deprecated@1.0.vendor \
+    
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
