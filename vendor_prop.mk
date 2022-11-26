eeddda0c59f7c62e863108c55e3d0f84687a24e7
xxseva44 <dev.xxseva44@gmail.com>
2022-11-25 20:40:55 -0500

    Drop netflix override

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -159,10 +159,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.disable.pq=true \
     vendor.video.disable.ubwc=1 \
     vidc.enc.dcvs.extra-buff-count=2
-
-# Netflix
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.netflix.bsp_rev=Q660-13149-1
     
 # OpenGLES
 PRODUCT_PROPERTY_OVERRIDES += \
